76db4f3d5c486d24f7408df3f136b80feff47afd
Mike Zbrozek <mzbrozek@strava.com>
2022-06-29 07:22:54 -0600

    Added StorePicker component, discussed a few gotchyas when rendering elements.

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import { ReactDOM } from 'react'
+import { render } from 'react-dom'
+import StorePicker from './components/StorePicker'
 
-class StorePicker extends React.Component {
-    render () {
-        return <p>Hello Mike!</p>
-    }
-}
+
+render(<StorePicker/>, document.querySelector('#main'))
